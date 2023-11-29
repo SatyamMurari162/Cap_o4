9931789d4dbf5561389c8ac8a2f15e784e572a90
SATYAM MURARI <137689968+SatyamMurari162@users.noreply.github.com>
2023-11-29 22:26:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let [milliseconds, seconds] = [0, 0];
+let timeRef = document.querySelector(".timer-display");
+let int = null;
+
+document.getElementById("start-timer").addEventListener("click", () => {
+    if(int !== null) {
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer, 10);
+});
+
+document.getElementById("pause-timer").addEventListener("click", () => {
+    clearInterval(int);
+});
+
+document.getElementById("reset-timer").addEventListener("click", () => {
+    clearInterval(int);
+    [milliseconds, seconds] = [0, 0];
+    timeRef.innerHTML = "00 : 00  ";
+}); 
+
+function displayTimer() {
+    milliseconds += 10;
+    if(milliseconds == 100) {
+        milliseconds = 0;
+        seconds++;
+        if(seconds == 60) {
+            seconds = 0;
+        }
+    }
+
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = 
+        milliseconds < 10
+        ? "0" + milliseconds
+        : milliseconds < 10
+        ? "0" + milliseconds
+        : milliseconds;
+
+    timeRef.innerHTML = `${s} : ${ms}`;
+}
